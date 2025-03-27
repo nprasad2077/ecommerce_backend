e564dbf0bf0477fbd5e7a5ecc3ec1f67dffbca7d
HALO\gangs <nprasad2077@gmail.com>
2025-03-27 16:25:31 -0500

    make migrations complete

--- backend/urls.py
+++ backend/urls.py
@@ -22,12 +22,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Our base URLs
     path('api/products/', include('base.urls.product_urls')),
     path('api/users/', include('base.urls.user_urls')),
     path('api/orders/', include('base.urls.order_urls')),
-    # If you want the DRF built-in browsable API login:
-    path('api-auth/', include('rest_framework.urls')),
 ]
 
 if settings.DEBUG:

4dda38cab06d08be8c726035e1a47d5bd2910ce3
HALO\gangs <nprasad2077@gmail.com>
2025-03-31 21:58:28 -0500

    docker version 5

--- backend/settings.py
+++ backend/settings.py
@@ -17,17 +17,14 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = os.getenv("DEBUG", "0") == "1"
+# DEBUG = os.getenv("DEBUG", "0") == "1"
 SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3hbk-(!pdwk!!s6ma)51)l^_4xxxg3#ti@o3oq-e2zekf6atsb"
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 

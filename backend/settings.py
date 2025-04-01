82fa9273a53ba37150c104c4c2035c19d5822626
HALO\gangs <nprasad2077@gmail.com>
2025-03-31 21:56:55 -0500

    docker version 5

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-3hbk-(!pdwk!!s6ma)51)l^_4xxxg3#ti@o3oq-e2zekf6atsb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

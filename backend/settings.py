8a7cf8bc39fa426ab0df765491350dec5e100371
HALO\gangs <nprasad2077@gmail.com>
2025-03-31 23:46:23 -0500

    docker version 15

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["159.203.171.104", "159.203.171.104:8888", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["159.203.171.104", "159.203.171.104:8888", "localhost", "127.0.0.1", "django.server.nbaapi.com"]
 
 
 # Application definition

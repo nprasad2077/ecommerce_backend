8c7976557c27a0ef61a789d05ca0da1b4c2dd0a7
HALO\gangs <nprasad2077@gmail.com>
2025-03-31 23:02:17 -0500

    docker version 11

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -14,4 +14,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static'))
-
